af36c4c399b4e81d2e863d15c0ec291e9a811d34
Grady O'Connell <flipcoder@gmail.com>
2014-06-25 22:51:49 -0700

    new channel system

--- include/kit/channel/channel.h
+++ include/kit/channel/channel.h
@@ -26,6 +26,49 @@ class Channel:
                 self->run();
             });
         }
+        void run_once() {
+            while(true){
+                auto l = this->lock();
+                if(!m_Cmds.empty()){
+                    auto task = std::move(m_Cmds.front());
+                    m_Cmds.pop();
+                    task();
+                    return;
+                }
+                boost::this_thread::interruption_point();
+                boost::this_thread::yield();
+            }
+        }
+        bool try_run_once() {
+            auto l = this->lock(std::defer_lock);
+            if(l.try_lock() == -1 && !m_Cmds.empty())
+            {
+                auto task = std::move(m_Cmds.front());
+                m_Cmds.pop();
+                task();
+                return true;
+            }
+            return false;
+        }
+        void run_all() {
+            while(true)
+            {
+                {
+                    auto l = this->lock();
+                    if(!m_Cmds.empty())
+                    {
+                        auto task = std::move(m_Cmds.front());
+                        m_Cmds.pop();
+                        task();
+                    }
+                    else
+                        return; // task queue is empty
+                        
+                }
+                boost::this_thread::interruption_point();
+                boost::this_thread::yield();
+            }
+        }
         void run() {
             while(true)
             {
@@ -42,13 +85,27 @@ class Channel:
                 boost::this_thread::yield();
             }
         }
+        bool empty() const {
+            auto l = this->lock();
+            return m_Cmds.empty();
+        }
+        size_t size() const {
+            auto l = this->lock();
+            return m_Cmds.size();
+        }
+        size_t select() const {
+            auto l = this->lock(std::defer_lock);
+            if(l.try_lock() == -1)
+                return m_Cmds.size();
+            return 0;
+        }
         std::future<T> operator()(std::function<T()> f) {
             std::packaged_task<T()> func(std::move(f));
             while(true)
             {
                 {
                     auto l = this->lock();
-                    if(m_Buffered && m_Buffered < m_Cmds.size()){
+                    if(!m_Buffered || m_Buffered < m_Cmds.size()){
                         auto fut = func.get_future();
                         m_Cmds.emplace(std::move(func));
                         return fut;
@@ -59,6 +116,7 @@ class Channel:
             }
         }
     private:
+        
         void join() {
             if(m_Thread.joinable()) {
                 m_Thread.interrupt();
@@ -66,7 +124,7 @@ class Channel:
             }
         }
         
-        unsigned m_Buffered = 0;
+        size_t m_Buffered = 0;
         boost::thread m_Thread;
         std::queue<std::packaged_task<T()>> m_Cmds;
 };
--- tests/channel.test.cpp
+++ tests/channel.test.cpp
@@ -6,10 +6,12 @@ TEST_CASE("Channel","[channel]") {
 
     SECTION("basic channel") {
         Channel<void> chan;
+        REQUIRE(chan.empty());
         chan([]{});
+        REQUIRE(!chan.empty());
     }
         
-    SECTION("futures") {
+    SECTION("run_once w/ futures") {
         Channel<int> chan;
         auto fut0 = chan([]{
             return 0;
@@ -17,9 +19,39 @@ TEST_CASE("Channel","[channel]") {
         auto fut1 = chan([]{
             return 1;
         });
+        
+        REQUIRE(chan.size() == 2);
+        
+        REQUIRE(fut0.wait_for(std::chrono::milliseconds(100)) == 
+            std::future_status::timeout);
+
+        chan.run_once();
+        
         REQUIRE(fut0.get() == 0);
+        REQUIRE(fut1.wait_for(std::chrono::milliseconds(100)) == 
+            std::future_status::timeout);
+        REQUIRE(!chan.empty());
+        REQUIRE(chan.size() == 1);
+        
+        chan.run_once();
+        
         REQUIRE(fut1.get() == 1);
+        REQUIRE(chan.empty());
+        REQUIRE(chan.size() == 0);
     }
 
+    SECTION("run_all"){
+        Channel<void> chan;
+        
+        chan([]{});
+        chan([]{});
+        chan([]{});
+        
+        REQUIRE(chan.size() == 3);
+        
+        chan.run_all();
+        
+        REQUIRE(chan.size() == 0);
+    }
 }
 
