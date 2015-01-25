18ebeacda06618d49c660ec792a7e3e1ae9a9106
Grady O'Connell <flipcoder@gmail.com>
2015-01-24 17:03:45 -0800

    added stability test to monitor MX's core usage

---
+++ toys/src/stability.cpp
@@ -0,0 +1,24 @@
+#include "../../include/kit/async/async.h"
+#include "../../include/kit/log/log.h"
+#include <chrono>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    srand(time(NULL));
+    for(unsigned i=0;i<MX.size();++i)
+        MX[i].frequency(1.0f);
+    
+    for(unsigned i=0;i<10;++i)
+    {
+        auto c = rand() % MX.size();
+        MX[c].coro<void>([i,c]{
+            for(;;){
+                YIELD();
+            }
+        });
+    }
+    
+    return 0;
+}
+
