2b2c61e081ff353cbaa3feac7e58d0b543a63bd4
Grady O'Connell <flipcoder@gmail.com>
2015-03-28 22:14:49 -0700

    added vector ops

--- include/kit/math/vectorops.h
+++ include/kit/math/vectorops.h
@@ -4,9 +4,10 @@
 ////#include "common.h"
 //#include <glm/glm.hpp>
 //#include <glm/gtc/swizzle.hpp>
+#include <string>
 
-//namespace Vector
-//{
+namespace Vector
+{
 //    inline bool isZero2(const glm::vec2& v) {
 //        if( floatcmp(v.x, 0.0f) &&
 //            floatcmp(v.y, 0.0f))
@@ -20,6 +21,20 @@
 //            return true;
 //        return false;
 //    }
-//}
+    inline std::string to_string(const glm::vec3& v) {
+        return std::string("(") +
+            std::to_string(v.x) + ", " +
+            std::to_string(v.y) + ", " +
+            std::to_string(v.z) + ")";
+    }
+    inline std::string to_string(const glm::vec4& v) {
+        return std::string("(") +
+            std::to_string(v.x) + ", " +
+            std::to_string(v.y) + ", " +
+            std::to_string(v.z) + ", " +
+            std::to_string(v.w) + ")";
+    }
+    
+}
 
 #endif
