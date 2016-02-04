2d4f12415ec12744fda37c5fbbf434cedfb7b4b4
Grady O'Connell <flipcoder@gmail.com>
2016-02-04 09:26:02 -0800

    changed cache_as behavior

--- include/kit/cache/cache.h
+++ include/kit/cache/cache.h
@@ -25,6 +25,9 @@ class Cache:
         
         virtual ~Cache() {}
         
+        /*
+         * Optimize cache, keeping only resources in usage
+         */
         virtual void optimize() override {
             auto l = this->lock();
             for(auto itr = m_Resources.begin();
@@ -36,7 +39,33 @@ class Cache:
                     ++itr;
             }
         }
+
+        /*
+         * Cache resource, force type
+         */
+        template<class Cast>
+        std::shared_ptr<Cast> cache_as(const T& arg) {
+            auto l = this->lock();
+            arg = transform(arg);
+            if(m_Preserve && m_Preserve(arg)) {
+                return std::make_shared<Cast>(
+                    std::tuple<T, ICache*>(arg, this)
+                );
+            }
+            try{
+                return std::dynamic_pointer_cast<Cast>(m_Resources.at(arg));
+            }catch(...){
+                auto p = std::make_shared<Cast>(
+                    std::tuple<T, ICache*>(arg, this)
+                );
+                m_Resources[arg] = std::static_pointer_cast<Class>(p);
+                return p;
+            }
+        }
         
+        /*
+         * Cache resource, bypass parameter transformer
+         */
         virtual std::shared_ptr<Class> cache_raw(const T& arg) {
             auto l = this->lock();
             if(m_Preserve && m_Preserve(arg)) {
@@ -55,6 +84,9 @@ class Cache:
             }
         }
         
+        /*
+         * Cache resource and let factory choose type
+         */
         virtual std::shared_ptr<Class> cache(T arg) {
             auto l = this->lock();
             arg = transform(arg);
@@ -65,8 +97,11 @@ class Cache:
         //virtual void cache(const std::shared_ptr<Cast>& blah) {
         //}
         
+        /*
+         * Cache resource and attempt to cast resource to a given base type
+         */
         template<class Cast>
-        std::shared_ptr<Cast> cache_as(T arg) {
+        std::shared_ptr<Cast> cache_cast(T arg) {
             auto l = this->lock();
             arg = transform(arg);
             std::shared_ptr<Cast> p;
@@ -95,11 +130,19 @@ class Cache:
             return m_Resources.empty();
         }
 
+        /*
+         * Completely clear ownership of ALL resources in cache.
+         * Compare to optimize(), which clears only unused resources.
+         */
         virtual void clear() override {
             auto l = this->lock();
             m_Resources.clear();
         }
 
+        /*
+         * Allows transformer, a user-provided functor, to transform parameters
+         * to ones usable by the underlying factory.
+         */
         T transform(const T& t){
             auto l = this->lock();
             if(m_Transformer)
@@ -107,11 +150,19 @@ class Cache:
             else
                 return t;
         }
+        /*
+         * Registers the given functor as the transformer, a functor that can
+         * transform parameters to ones usable by the underlying factory.
+         */
         void register_transformer(std::function<T(const T&)> f) {
             auto l = this->lock();
             m_Transformer=f;
         }
         
+        /*
+         * Registers a preservation test, a functor that decides whether or not
+         * to cache the resource, given the object's parameters.
+         */
         void register_preserver(std::function<bool(const T&)> f) {
             auto l = this->lock();
             m_Preserve = f;
@@ -121,7 +172,7 @@ class Cache:
 
         std::unordered_map<T, std::shared_ptr<Class>> m_Resources;
         std::function<T(const T&)> m_Transformer;
-        std::function<bool(const T&)> m_Preserve; // cache this?
+        std::function<bool(const T&)> m_Preserve;
 };
 
 #endif
