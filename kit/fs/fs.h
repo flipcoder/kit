e13cd7c07774d3145a3b823cb6fa7320114319b3
Grady O'Connell <flipcoder@gmail.com>
2016-09-22 17:08:10 -0700

    fs configpath()

--- kit/fs/fs.h
+++ kit/fs/fs.h
@@ -7,6 +7,12 @@
 
 namespace fs {
 
+#ifdef _WIN32
+#define SEP "\\"
+#else
+#define SEP "/"
+#endif
+
     inline std::string homedir() {
         const char* homedir = getenv("HOME");
         if(!homedir)
@@ -14,6 +20,36 @@ namespace fs {
         if(!homedir)
             K_ERROR(GENERAL, "Unable to locate home directory.");
         return homedir;
+    }
+
+    inline std::string configdir(std::string apppath = std::string()) {
+        std::string r;
+        bool add_config=false;
+        const char* cdir = getenv("XDG_CONFIG_PATH");
+        if(!cdir){
+            cdir = getenv("HOME");
+            add_config=true;
+        }
+        if(!cdir){
+            cdir = getenv("HOMEPATH");
+            add_config=true;
+        }
+        if(!cdir){
+            K_ERROR(GENERAL, "Unable to locate home directory.");
+        }
+        if(add_config)
+        {
+#ifdef _WIN32
+            r = std::string(cdir);
+#else
+            r = std::string(cdir) + SEP + ".config";
+#endif
+        }
+        else
+            r = cdir;
+        if(not apppath.empty())
+            return r + SEP + apppath;
+        return r;
     }
 }
 
